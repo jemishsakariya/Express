1308695a1832b923fd7b280b2be2bc7382d6efb3
jemishsakariya <jemishsakariya22@gmail.com>
2024-05-02 15:41:46 +0530

    format indexJs

--- express_CRUD/index.js
+++ express_CRUD/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const app = express();
-
 const fs = require("fs");
 const path = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
+const router = require("./routes/index");
 const config = require("./utils/config");
+
 const PORT = config.PORT;
+const app = express();
 
 app.use(express.json());
 
-const morgan = require("morgan");
 // custom format - 1
 // app.use(
 //   morgan(":method :url :status :res[content-length] - :response-time ms")
@@ -54,18 +57,15 @@ const morgan = require("morgan");
 
 app.use(morgan("dev"));
 
-const helmet = require("helmet");
 app.use(helmet());
 
-// const cors = require("cors");
 // const corsOptions = {
-  // origin: "http://127.0.0.1:5500", // all api is only available for localhost with port=5500 other can not access this app apis
-  // origin: "*", // everyone can acsess api
-  // optionsSuccessStatus: 200, status code success(200) then they able to access
+// origin: "http://127.0.0.1:5500", // all api is only available for localhost with port=5500 other can not access this app apis
+// origin: "*", // everyone can acsess api
+// optionsSuccessStatus: 200, status code success(200) then they able to access
 // };
 // app.use(cors(corsOptions));
 
-const router = require("./routes/index");
 app.use("/api", router);
 
 app.get("/", function (req, res) {
