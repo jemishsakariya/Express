8d9cfadf0a9adec9879ed05222a148db6f8a3631
jemishsakariya <jemishsakariya22@gmail.com>
2024-05-07 16:20:21 +0530

    add and commit together (git commit -am " add and commit together\)

--- express_CRUD/eslint.config.mjs
+++ express_CRUD/eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
       semi: ["error", "always"],
     },
   },
-  { ignores: ["utils/config.js"] },
+  { ignores: ["utils/config.js"] }, 
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
