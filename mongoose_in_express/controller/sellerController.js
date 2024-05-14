348c2e85e22c030866dc14c440b2d91dcaa7e31e
jemishsakariya <jemishsakariya22@gmail.com>
2024-05-14 10:23:34 +0530

    seller

--- mongoose_in_express/controller/sellerController.js
+++ mongoose_in_express/controller/sellerController.js
@@ -1,3 +1,29 @@
+const Car = require("../models/carModel");
 const Seller = require("../models/sellerModel");
 
-exports.addSeller = async (req, res) => {};
+exports.addSeller = async (req, res) => {
+  try {
+    const sName = req.body.sName;
+    const sCity = req.body.sCity;
+    const aCarsIDs = req.body.aCarsIDs;
+
+    for (const iCarID of aCarsIDs) {
+      const oCar = await Car.findById(iCarID);
+      if (!oCar) {
+        return res
+          .status(400)
+          .json({ sMessage: `Car Not Found with id: ${iCarID}` });
+      }
+    }
+
+    const seller = await Seller.create({ sName, sCity, aCars: aCarsIDs });
+
+    return res
+      .status(200)
+      .json({ sMessage: "Seller Added Successfully", oSeller: seller });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sMessage: "Internal Server Error", sError: error.message });
+  }
+};
--- mongoose_in_express/validator/sellerValidator.js
+++ mongoose_in_express/validator/sellerValidator.js
@@ -1,1 +1,17 @@
-exports.isValidSaller = (req, res, next) => {};
+exports.isValidSaller = (req, res, next) => {
+  try {
+    const sName = req.body.sName;
+    const sCity = req.body.sCity;
+    const aCarsIDs = req.body.aCarsIDs;
+
+    if (!sName || !sCity || !aCarsIDs) {
+      return res.status(404).json({ sMessage: "Please Fill All The Fields!" });
+    }
+
+    return next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sMessage: "Internal Server Error", sError: error.message });
+  }
+};
