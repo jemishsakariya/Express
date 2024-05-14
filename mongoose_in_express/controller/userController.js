e0c9aa8f5d09420efde9256e1972c5eea247fc0d
jemishsakariya <jemishsakariya22@gmail.com>
2024-05-14 10:28:03 +0530

    Users

--- mongoose_in_express/controller/userController.js
+++ mongoose_in_express/controller/userController.js
@@ -1,3 +1,18 @@
 const User = require("../models/userModel");
 
-exports.addUser = async (req, res) => {};
+exports.addUser = async (req, res) => {
+  try {
+    const sName = req.body.sName;
+    const sCity = req.body.sCity;
+
+    const user = await User.create({ sName, sCity });
+
+    return res
+      .status(200)
+      .json({ sMessage: "User Added Successfully", oUser: user });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sMessage: "Internal Server Error", sError: error.message });
+  }
+};
--- mongoose_in_express/validator/userValidator.js
+++ mongoose_in_express/validator/userValidator.js
@@ -1,1 +1,16 @@
-exports.isValidUser = (req, res, next) => {};
+exports.isValidUser = (req, res, next) => {
+  try {
+    const sName = req.body.sName;
+    const sCity = req.body.sCity;
+
+    if (!sName || !sCity) {
+      return res.status(404).json({ sMessage: "Please Fill All The Fields!" });
+    }
+
+    return next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sMessage: "Internal Server Error", sError: error.message });
+  }
+};
