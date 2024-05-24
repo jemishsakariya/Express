e3deab2fffc44a731894757b769577b20ab07b66
jemishsakariya <jemishsakariya22@gmail.com>
2024-05-24 15:12:16 +0530

    In which city cars are most sold

--- mongoose_in_express/controller/adminController.js
+++ mongoose_in_express/controller/adminController.js
@@ -24,7 +24,85 @@ exports.totalSoldCars = async (_req, res) => {
       { $group: { _id: null, countOfTotalCarsSold: { $sum: 1 } } },
     ]);
 
+    // const totalSoldCars = await Trasaction.countDocuments();
+
     return res.status(200).json({ totalSoldCars });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sMessage: "Internal Server Error", sError: error.message });
+  }
+};
+
+exports.carSoldMostInCity = async (_req, res) => {
+  try {
+    const carsoldmostinCity = await Trasaction.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "iUsersID",
+          foreignField: "_id",
+          as: "userCollection",
+        },
+      },
+      {
+        $unwind: "$userCollection",
+      },
+      {
+        $group: {
+          _id: "$userCollection.sCity",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: {
+          count: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ]);
+
+    return res.status(200).json({ carsoldmostinCity });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ sMessage: "Internal Server Error", sError: error.message });
+  }
+};
+
+exports.mostSoldCar = async (_req, res) => {
+  try {
+    const mostSoldCar = await Trasaction.aggregate([
+      {
+        $lookup: {
+          from: "cars",
+          localField: "iCarsID",
+          foreignField: "_id",
+          as: "userCollection",
+        },
+      },
+      {
+        $unwind: "$userCollection",
+      },
+      {
+        $group: {
+          _id: "$userCollection.sName",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: {
+          count: -1,
+        },
+      },
+      {
+        $limit: 1,
+      },
+    ]);
+
+    return res.status(200).json({ mostSoldCar });
   } catch (error) {
     return res
       .status(500)
--- mongoose_in_express/routes/adminRouter.js
+++ mongoose_in_express/routes/adminRouter.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 const { isValidAdmin } = require("../validator/adminValidator");
-const { addAdmin, totalSoldCars } = require("../controller/adminController");
+const { addAdmin, totalSoldCars, carSoldMostInCity, mostSoldCar } = require("../controller/adminController");
 
 router.post("/", isValidAdmin, addAdmin);
 router.get("/totalsoldcars", totalSoldCars);
+router.get("/carsoldmostincity", carSoldMostInCity);
+router.get("/mostsoldcar", mostSoldCar);
 
 module.exports = router;
